fbc946cabf4c198a5b1984ae01a7eea7f3ac8bf1
David Molineus <david.molineus@netzmacht.de>
2019-03-19 12:07:18 +0100

    Add german translation.

---
+++ src/Resources/contao/languages/de/hofff_dca_notification.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['hofff_dca_notification']['hofff_dca_notification_legend']          = 'Benachrichtigung';
+$GLOBALS['TL_LANG']['hofff_dca_notification']['hofff_dca_notification_send'][0]         = 'Benachrichtigung senden';
+$GLOBALS['TL_LANG']['hofff_dca_notification']['hofff_dca_notification_send'][1]         = 'Benachrichtigung wird beim Speichern versandt.';
+$GLOBALS['TL_LANG']['hofff_dca_notification']['hofff_dca_notification_notification'][0] = 'Benachrichtigung';
+$GLOBALS['TL_LANG']['hofff_dca_notification']['hofff_dca_notification_notification'][1] = 'Bitte wählen Sie eine Benachrichtung aus, die versandt werden soll.';
---
+++ src/Resources/contao/languages/de/tl_nc_notification.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['tl_nc_notification']['hofff_dca_notification_table'][0]          = 'Tabelle';
+$GLOBALS['TL_LANG']['tl_nc_notification']['hofff_dca_notification_table'][1]          = 'Bitte wählen Sie eine DCA-basierte Tabelle aus';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['hofff_dca_notification']           = 'DCA Benachrichtigung';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['hofff_dca_notification_submit'][0] = 'DCA Submit Benachrichtigung';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['hofff_dca_notification_submit'][1] = 'DCA Benachrichtigung, die beim Auslösen des <em>onsubmit_callback</em> versandt wird.';
